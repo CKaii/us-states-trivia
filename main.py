1fc4f63fcfc8a3d3c2772bef3736cd92b3288b02
unknown <kevin.cheung.kai@gmail.com>
2021-03-19 18:59:28 -0400

    Added checking guess with states data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title('U.S States Game')
@@ -6,9 +7,9 @@ image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
+answer_state = screen.textinput(title='Guess the state', prompt='What\'s another states name?').title()
+data = pandas.read_csv('50_states.csv')
+state_name = data['state'].to_string()
 
-
-
-
-
-screen.exitonclick()
+if answer_state in state_name:
+    print(answer_state)
